97916d59cf16b96820af9979b1e19339f33eea13
umakondeti <kondetiuma@gmail.com>
2017-04-26 05:19:26 -0400

    Initial commit

---
+++ app/src/main/java/movers/ngagroupinc/com/todo/Item.java
@@ -0,0 +1,24 @@
+package movers.ngagroupinc.com.todo;
+
+/**
+ * Created by user1 on 20-Feb-17.
+ */
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
